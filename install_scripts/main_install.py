2aa654258b32e8b3a9731a2051aff35f7085ff43
SantosJGND <dourado.jns@gmail.com>
2026-05-11 12:01:15 +0100

    make centrifuge only db install

--- install_scripts/main_install.py
+++ install_scripts/main_install.py
@@ -493,7 +493,7 @@ class main_setup:
         ######################### centrifuge (download) ###############################
 
         for db_name, entry in list_enabled_databases('centrifuge'):
-            if db_name in ["viral", "bacteria"]:
+            if db_name in ["ignore"]:#["viral", "bacteria"]:
                 install_success = sofprep.centrifuge_install(dbname=self.organism)
                 centlib = f"refseq-{self.organism}.dust.fna.gz"
                 fasta_key = "centrifuge"
